283e794f819eb84c5a28fdddfbda5ae7a9ba0df5
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2024-07-02 13:45:29 +0100

    Adding logo

--- popup/components/HeaderSection.tsx
+++ popup/components/HeaderSection.tsx
@@ -1,5 +1,6 @@
 import { Settings2 } from "lucide-react"
 
+import Logo from "~components/logo"
 import { ModeToggle } from "~components/mode-toggle"
 import { Button } from "~components/ui/button"
 import {
@@ -11,8 +12,8 @@ import {
 
 const HeaderSection = () => {
   return (
-    <div className="flex justify-between items-center bg-green-800 text-xl text-white font-medium p-4">
-      <div>GreenCheck Header</div>
+    <div className="flex justify-between items-center bg-emerald-800 text-xl text-white font-medium py-4 px-6">
+      <Logo />
       <div className="flex gap-1">
         <TooltipProvider>
           <Tooltip>
@@ -21,7 +22,7 @@ const HeaderSection = () => {
                 asChild
                 variant="ghost"
                 size="icon"
-                className="hover:bg-green-900 hover:text-white">
+                className="hover:bg-emerald-900 hover:text-white">
                 <a
                   href={`chrome-extension://${process.env.PLASMO_PUBLIC_CRX_ID}/options.html`}
                   target="_blank">
